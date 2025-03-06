c4c1e3576e529ea783eef98a373deef4a4ef764e
Yogesh Kumar <yogeshkumar.n1234@gmail.com>
2025-03-06 15:28:11 +0530

    after_mistake_count

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var start =0;
 let mistakes = 0;
-let mistakeTag = document.querySelector(".wrapper .content .mistakes span");
+let mistakeTag = document.querySelector(".mistake span");
 const paragraphs = [
     "I found a love for me oh darling just dive right in and follow my lead well i found a girl beautiful and sweet oh i never knew you were the someone waitin for me cause we were just kids when we fell in love not knowin what it was i will not give you up this time oh darling just kiss me slow your heart is all i own and in your eyes youre holding mine baby im dancin in the dark with you between my arms barefoot on the grass while listenin to our favourite song when you said you looked a mess i whispered underneath my breath but you heard it darling you look perfect tonight well i found a woman stronger than anyone i know she shares my dreams i hope that someday ill share her home i found a love to carry more than just my secrets to carry love to carry children of our own we are still kids but were so in love fightin against all odds i know well be alright this time darling just hold my hand be my girl ill be your man i see my future in your eyes oh baby im dancin in the dark with you between my arms barefoot on the grass while listenin to our favourite song when i saw you in that dress lookin so beautiful i dont deserve this darling you look perfect tonight no no no mm oh baby im dancin in the dark with you between my arms barefoot on the grass oh listenin to our favourite song i have faith in what i see now i know i have met an angel in person and she looks perfect though i dont deserve this you look perfect tonight"
 ];
@@ -18,7 +18,7 @@ function randomParagraph() {
     });
 
     document.addEventListener("keydown", () => inpField.focus());
-    typingText.addEventListener("click", () => inpField.focus());
+
 }
 
 function initTyping(event) {
@@ -30,17 +30,23 @@ function initTyping(event) {
     let typedChar = inpField.value[charIndex];
 
     if (event.inputType === "deleteContentBackward") { 
+        
+        
         // **Handle Backspace Correctly**
         if (charIndex > 0) { 
             charIndex--; 
+            if (characters[charIndex].classList.contains("incorrect")){
+                mistakes--;
+                mistakeTag.innerText = mistakes;
+            }
             
             // Skip spaces when moving back
             // while (charIndex > 0 && characters[charIndex].innerText === " ") {
             //     charIndex--;
             // }
-
             characters[charIndex].classList.remove("correct", "incorrect", "active");
         }
+        
     } else {
         if (typedChar == null) return;
 
@@ -48,8 +54,10 @@ function initTyping(event) {
             characters[charIndex].classList.add("correct");
             characters[charIndex].classList.remove("incorrect");
         } else {
+
             song('wrong');
             mistakes++;
+            mistakeTag.innerText = mistakes;
             characters[charIndex].classList.add("incorrect");
             characters[charIndex].classList.remove("correct");
         }
@@ -64,7 +72,7 @@ function initTyping(event) {
         // If reached the end, remove active from last character
         characters[charIndex - 1]?.classList.remove("active");
     }
-    mistakeTag.innerText = mistakes;
+    
 }
 
 randomParagraph();
